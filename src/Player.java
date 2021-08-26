a08eb163aa74aabe7220d80daf038e08d1934d09
Igor Lazarev <lazermann111@gmail.com>
2021-08-26 21:16:28 +0300

    nice OOP version

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 /**
@@ -5,6 +8,9 @@ import java.util.Scanner;
  * the standard input according to the problem statement.
  **/
 class Player {
+
+    static List<FactoryRoute> factoryRoutes = new LinkedList<>();
+    static List<GameEntity> entities = new LinkedList<>();
 
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
@@ -14,10 +20,15 @@ class Player {
             int factory1 = scanner.nextInt();
             int factory2 = scanner.nextInt();
             int distance = scanner.nextInt();
+
+            factoryRoutes.add(new FactoryRoute(factory1, factory2, distance));
+
         }
 
         // game loop
         while (true) {
+
+
             int entityCount = scanner.nextInt(); // the number of entities (e.g. factories and troops)
             for (int i = 0; i < entityCount; i++) {
                 int entityId = scanner.nextInt();
@@ -27,6 +38,15 @@ class Player {
                 int arg3 = scanner.nextInt();
                 int arg4 = scanner.nextInt();
                 int arg5 = scanner.nextInt();
+
+                if (Objects.equals(entityType, "TROOP")) {
+                    entities.add(new Troops(entityId, arg4, Owner.fromInt(arg1), arg2, arg3, arg5));
+                }
+
+                else if (Objects.equals(entityType, "FACTORY")) {
+                    entities.add(new Factory(entityId, arg2, Owner.fromInt(arg1), arg3));
+                }
+
             }
 
             // Write an action using System.out.println()
@@ -39,7 +59,6 @@ class Player {
     }
 
 
-
     enum Owner {
         ME(1),
         OPPONENT(-1),
@@ -49,11 +68,24 @@ class Player {
             this.code = code;
         }
 
-        int code;
+        static Owner fromInt(int value) {
+            switch (value) {
+                case -1:
+                    return OPPONENT;
+                case 1:
+                    return ME;
+                case 0:
+                    return NEUTRAL;
+                default:
+                    throw new IllegalArgumentException("No such owner type!");
+            }
+        }
+
+        private int code;
     }
 
     //todo (ilazarev) come up with better class name!
-    abstract class GameEntity {
+    static abstract class GameEntity {
         protected int id;
         protected int troopsCount;
         protected Owner owner;
@@ -90,7 +122,7 @@ class Player {
 
     }
 
-    class Factory extends GameEntity {
+    static class Factory extends GameEntity {
         private int production;
 
         public Factory(int id, int troopsCount, Owner owner, int production) {
@@ -118,14 +150,16 @@ class Player {
         }
     }
 
-    class Troops extends GameEntity {
+    static class Troops extends GameEntity {
         private int sourceId;
         private int destinationId;
+        private int arrivingIn;
 
-        public Troops(int id, int troopsCount, Owner owner, int sourceId, int destinationId) {
+        public Troops(int id, int troopsCount, Owner owner, int sourceId, int destinationId, int arrivingIn) {
             super(id, troopsCount, owner);
             this.sourceId = sourceId;
             this.destinationId = destinationId;
+            this.arrivingIn = arrivingIn;
         }
 
         public int getSourceId() {
@@ -153,6 +187,18 @@ class Player {
                     ", sourceId=" + sourceId +
                     ", destinationId=" + destinationId +
                     '}';
+        }
+    }
+
+    static class FactoryRoute {
+        int sourceId;
+        int destinationId;
+        int distance;
+
+        public FactoryRoute(int sourceId, int destinationId, int distance) {
+            this.sourceId = sourceId;
+            this.destinationId = destinationId;
+            this.distance = distance;
         }
     }
 }
